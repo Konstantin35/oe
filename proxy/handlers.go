a1fe806270d701493e32d4f08fb64e9ee322536d
filod lin <filod@qq.com>
2017-09-14 13:43:06 +0800

    TESTLOG

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -92,7 +92,7 @@ func (s *ProxyServer) handleSubmitRPC(cs *Session, login, id string, params []st
 		}
 		return false, nil
 	}
-	//log.Printf("Valid share from %s@%s", login, cs.ip)
+	log.Printf("TESTLOG: Valid share from %s@%s", login, cs.ip)
 
 	if !ok {
 		return true, &ErrorReply{Code: -1, Message: "High rate of invalid shares"}
